c085101399289594f3fa66a64ebbe5947218e542
DESKTOP-ROER1NG\まっちゃん <ilovegames.ilikesplatoon@gmail.com>
2021-06-24 10:57:42 +0900

    シーン遷移用テストシーン追加　シーン遷移用ボタン追加

---
+++ NurtureAircraft/Assets/Scripts/test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(0, 0, speed);
+    }
+}
